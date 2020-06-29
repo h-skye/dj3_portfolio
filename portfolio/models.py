d235f8027173a35f02d4966455ce4645e07edf3c
YY-Huang <yansquaredh@gmail.com>
2020-06-29 12:21:22 -0400

    CharField syntax error fix

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.Charfield(max_length=100)
-    description = models.Charfield(max_length=250)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images')
     url = models.URLField(blank=True)
