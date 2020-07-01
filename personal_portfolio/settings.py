acf06c2ed27bc31ef44a6559b1c2badf16551440
Yan <yansquaredh@gmail.com>
2020-07-01 20:26:15 +0000

    removed static additional files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'us#w_20p55g^k=&ns2m&gx^=*ygu(fpb(a7b_$-9(^-0exzr1y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yannersz.pythonanywhere.com']
 
 
 # Application definition
@@ -120,9 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "portfolio/static")
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = ("/portfolio/static",)
+STATICFILES_DIRS = ("portfolio/static",)
