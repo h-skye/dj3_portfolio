ab78e34e3a5d7f2a6b606018e4fd56b49dddf697
YY-Huang <yansquaredh@gmail.com>
2020-07-01 11:10:50 -0400

    truncate blog characters to 100

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Blog
 # Create your views here.
 def all_blogs(request):
     # blogs = Blog.objects.all()
-    blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date') #[:5]
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
 
 def detail(request, blog_id):
